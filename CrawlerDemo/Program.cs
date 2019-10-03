b8c320afc877035d14e0665170a5e8f14357a679
Vitor Myra Moreira <vmoreira@prodesp-see.local>
2019-10-03 12:37:54 -0300

    robôs para varrer sites

---
+++ CrawlerDemo/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using HtmlAgilityPack;
+using System.Text;
+using System.Data.Odbc;
+using System.Threading.Tasks;
+using CrawlerDemo;
+
+namespace CrawlerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            startCrawlerasync();
+            Console.ReadLine();
+            
+        }
+
+        private static async Task startCrawlerasync()
+        {
+           
+            //the url of the page we want to test
+            var url = "https://dolarhoje.com/bitcoin-sv-hoje/";
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            // a list to add all the list of cars and the various prices 
+            var cars = new List<Carro>();
+            var divs =
+            htmlDocument.DocumentNode.Descendants("div")
+                .Where(node => node.GetAttributeValue("id", "").Equals("cotacao")).ToList();
+                       
+            foreach(var div in divs)
+            {
+                var teste = div.Descendants("input")
+                    .Where(node => node.GetAttributeValue("id", "").Equals("nacional")).ToList();
+
+                string preco = teste[0].OuterHtml.Replace("<input type=\"text\"", "").Replace("id=\"nacional\"", "").Replace("value=\"", "").Replace("\">", "").Trim();
+
+                //var car = new Carro
+                //{
+
+                //    //Model = div.Descendants("input").FirstOrDefault().InnerText,
+                //    //Price = div.Descendants("span").FirstOrDefault().InnerText,
+                //    //Link = div.Descendants("a").FirstOrDefault().ChildAttributes("href").FirstOrDefault().Value,
+                //    //ImageUrl = div.Descendants("img").FirstOrDefault().ChildAttributes("src").FirstOrDefault().Value
+                //};
+
+                //cars.Add(car);              
+             }
+            // Connection string 
+            string MyConnection = "DRIVER={MySQL ODBC 3.51 Driver};Server=localhost;Database=crawlerdemo;User Id=root;Password=";
+            //string MyConnection = "datasource=localhost;username=root;password=";  
+            OdbcConnection con = new OdbcConnection(MyConnection);
+            con.Open();
+
+            try
+            {
+                int count = cars.Count;
+                foreach(var item in cars)
+                {
+                    for(int i = 0; i < count; i++)
+                    {
+                        string query = "insert into carinfor(Model,Price,Link,ImageUrl) value(?,?,?,?);";
+                        OdbcCommand cmd = new OdbcCommand(query, con);
+                        cmd.Parameters.Add("?Model", OdbcType.VarChar).Value = cars[i].Model;
+                        cmd.Parameters.Add("?Price", OdbcType.VarChar).Value = cars[i].Price;
+                        cmd.Parameters.Add("?Link", OdbcType.VarChar).Value = cars[i].Link;
+                        cmd.Parameters.Add("?ImageUrl", OdbcType.VarChar).Value = cars[i].ImageUrl;
+                        OdbcDataReader reader = cmd.ExecuteReader();
+                        reader.Close();
+                    }
+
+                    count = 0;
+                }
+            }
+            catch(Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }  
+           
+            con.Close();
+            Console.WriteLine("Successful....");
+            Console.WriteLine("Press Enter to exit the program...");
+            ConsoleKeyInfo keyinfor = Console.ReadKey(true);
+            if(keyinfor.Key == ConsoleKey.Enter)
+            {
+                System.Environment.Exit(0);
+            }
+
+        }
+       
+    }
+}
---
+++ README.md
@@ -0,0 +1,40 @@
+# JAC.WebCrawling
+
+
+este exemplo pratico sobre o WebCrawling, que tem se tornado cada vez mais util para os programadores,estarei usando
+[] - 
+HtmlAgilityPack
+Net.Http
+
+O que é o rastreio da Web?
+Crawling é o processo em virtude do qual os motores de busca coletam informações sobre sites na world wide web. Ele também pode ser usado para automatizar tarefas de manutenção em um site, como verificar links ou validar código HTML
+
+Rastejar basicamente significa seguir um caminho. É por isso que muitos desenvolvedores de sites fornecem um mapa do site para facilitar a navegação e o rastreamento em seus sites.
+
+Como os mecanismos de busca recomendam algumas páginas dos trilhões que existem? A resposta é o rastreamento da web
+
+O que é um rastreador da Web?
+Um rastreador da Web também conhecido como web spider ou webrobot é um programa ou script automatizado que navega na World Wide Web de maneira metodológica e automatizada. Esse processo é chamado de rastreamento da Web ou Spidering. Muitos sites legítimos, em particular os mecanismos de pesquisa, usam o Spidering como um meio de fornecer dados atualizados para análises.
+
+Os rastreadores da Web são programas de computador que examinam a Web, "lêem" tudo o que encontram. Os rastreadores da Web também são conhecidos como spiders, bots e indexadores automáticos. Esses rastreadores verificam as páginas da Web para ver quais palavras elas contêm e onde essas palavras são usadas. O rastreador transforma suas descobertas em um índice gigante
+
+A finalidade do rastreamento da Web é tipicamente para fins de indexação da Web ( spidering da Web ).
+
+Web Indexing
+Web Indexing refere-se a vários métodos para indexar o conteúdo de um site ou da Internet como um todo. O índice é basicamente uma grande lista de palavras e as páginas da Web que as apresentam. Os rastreadores da Web verificam a Web regularmente para que sempre tenham um índice atualizado da web. Então, quando você solicita a um site de busca por páginas sobre hipopótamos, o mecanismo de pesquisa verifica seu índice e fornece uma lista de páginas que mencionam hipopótamos.
+
+Quando o Google visita seu site para fins de rastreamento. Esse processo é feito pelo rastreador do Google Spider e após o rastreamento ter sido feito, os resultados são colocados no índice do Google (ou seja, na pesquisa na Web)
+
+Algumas ferramentas de rastreamento da Web para diferentes plataformas
+Nutch
+Escamoso
+GRUB
+Ccrawler
+Opese
+Googlebot
+Arachnode.net
+JSpider
+Arale
+WebLech
+depois de toda a introdução ao que o web crawling é, vamos agora entrar na codificação real usando c # e visual studio 2013 seguindo estes passos. lembre-se que para este tutorial nós iremos obter os modelos de carro, link, URL da imagem e preços, mas depois de dominar este processo você pode obter qualquer informação em qualquer site, desde que eles estejam disponíveis no site, mas se você tiver algum desafio chegar até mim para que possamos olhar juntos.
+
